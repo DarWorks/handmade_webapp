58392a64b84568b19c9937b0c5085f55570ac8e2
jonUCSC <jon@LAPTOP-ENF3SU60.localdomain>
2022-05-02 13:31:52 -0700

    finished template

--- controllers.py
+++ controllers.py
@@ -41,13 +41,17 @@ def index():
         my_callback_url = URL('my_callback', signer=url_signer),
     )
 
-@action('product/<product_id:int>')
+@action('product/<seller_name>/<product_id:int>')
 @action.uses('product.html')
-def product(product_id=None):
+def product(seller_name=None, product_id=None):
     assert product_id is not None
+    assert seller_name is not None
+    # TODO: grab product data from DB using product id and confirm that seller name matches
+    # Then serialize the data in the format that is used in the html template like shown below
     return dict(
         product = dict(
             name="Product Name",
+            seller="SellerUsername",
             description="This is the product description. ",
             images=("images/product/image1.png", "images/product/image2.png")
         ),
