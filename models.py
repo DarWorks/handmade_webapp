a327ea6ac5604623e2fb88d17623c1d5aacc7616
Yashaswi Bathula <Yashaswi@PurpleResonance.local>
2022-05-05 01:08:24 -0700

    reverting back to initial  models.py

--- models.py
+++ models.py
@@ -2,11 +2,7 @@
 This file defines the database models
 """
 
-from __future__ import unicode_literals
 import datetime
-# from importlib.metadata import requires
-
-from pkg_resources import require
 from .common import db, Field, auth
 from pydal.validators import *
 
@@ -18,36 +14,6 @@ def get_time():
     return datetime.datetime.utcnow()
 
 
-# userProfile DB table
-db.define_table(
-    'userProfile',
-    Field('first_name','text',default="", requires=IS_NOT_EMPTY()),
-    Field('last_name','text',default="", requires=IS_NOT_EMPTY()),
-    Field('user_email', default=get_user_email),
-    Field('username','text', unique=True, requires=IS_NOT_EMPTY()),  # 1* Check comments below for details w.r to 'uniqie' attribute
-    Field('balance','float', default=0, requires=IS_FLOAT_IN_RANGE(0, 1e6)), # 2* Check below
-    Field('isPersonlized','boolean', default=False),
-
-)
-# 1* unique=TRUE ensures the field entry is unique, but does not display an error prompt within implemented forms
-# the error prompt is implemented here:
-db.userProfile.username.requires= (IS_NOT_IN_DB(db, 'userProfile.username'))
-# Not 100% sure if this is the correct argument syntax for validating unique username in the DB, but
-# it will become obvious when implementing the respective form.
-#src: https://www.web2pyref.com/reference/is_not_in_db-validator
-
-
-# 2* kinda just tossed this in here, are we going to attribute this here or in a potential,
-# connected 'wallet' DB table? LMK
-
-
-
-
-# making the userProfile id read/write inaccessible to user
-db.userProfile.id.readable = db.userProfile.id.writable = False
-
-
-
 ### Define your table below
 #
 # db.define_table('thing', Field('name'))
@@ -55,3 +21,5 @@ db.userProfile.id.readable = db.userProfile.id.writable = False
 ## always commit your models to avoid problems later
 
 db.commit()
+
+
