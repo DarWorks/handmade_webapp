976796dac92e0ae96b299ecdf4f63976c4faa08a
Aaron Chen <achen180@ucsc.edu>
2022-05-15 02:10:27 -0700

    better code

--- static/js/aboutus.js
+++ static/js/aboutus.js
@@ -3,60 +3,45 @@ let app = {};
 let init = (app) => {
     app.data = {
         aaron: true,
-        aarontext: false,
         amaan: true,
-        amaantext: false,
         darien: true,
-        darientext: false,
         jonathan: true,
-        jonathantext: false,
         yashaswi: true,
-        yashaswitext: false,
     }
 
     app.show = function(x) {
         if (x == 1) {
             app.vue.aaron = false;
-            app.vue.aarontext = true;
         }
         else if (x == 2) {
             app.vue.amaan = false;
-            app.vue.amaantext = true;
         }
         else if (x == 3) {
             app.vue.darien = false;
-            app.vue.darientext = true;
         }
         else if (x == 4) {
             app.vue.jonathan = false;
-            app.vue.jonathantext = true;
         }
         else if (x == 5) {
             app.vue.yashaswi = false;
-            app.vue.yashaswitext = true;
         }
     };
 
     app.hide = function(x) {
         if (x == 1) {
             app.vue.aaron = true;
-            app.vue.aarontext = false;
         }
         else if (x == 2) {
             app.vue.amaan = true;
-            app.vue.amaantext = false;
         }
         else if (x == 3) {
             app.vue.darien = true;
-            app.vue.darientext = false;
         }
         else if (x == 4) {
             app.vue.jonathan = true;
-            app.vue.jonathantext = false;
         }
         else if (x == 5) {
             app.vue.yashaswi = true;
-            app.vue.yashaswitext = false;
         }
     };
 
