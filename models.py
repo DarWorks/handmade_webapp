a312ba06b54c4ff8a2fd8f378f22c14bbd217f21
DarWorks <59351227+DarWorks@users.noreply.github.com>
2022-06-05 16:51:39 -0700

    Updating (#79)
    * Product pages now accounts for unavailable items in db (#76)
    
    * Jon/check bug (#77)
    
    * init
    
    * now it checks rating
    
    * mnpr change
    
    * about and faq (#73)
    
    * stuff about product and profile pages
    
    * afd
    
    * Update README.md
    
    * Added content and organization to READ.ME
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Dar/small features (#78)
    
    * Product pages now accounts for unavailable items in db
    
    * Stores and displays profile pics on Profile page
    
    * Comments and reviews display profile pics
    
    Co-authored-by: jonUCSC <103705981+jonUCSC@users.noreply.github.com>
    Co-authored-by: achen180 <103001244+achen180@users.noreply.github.com>
    Co-authored-by: ybathula707 <60422402+ybathula707@users.noreply.github.com>

--- models.py
+++ models.py
@@ -79,7 +79,6 @@ db.define_table(
 
 db.define_table(
     'order_history',
-    Field('sellerid', 'integer', requires=IS_NOT_EMPTY()),
     Field('buyerid', 'integer', requires=IS_NOT_EMPTY()),
     Field('productid', 'integer', requires=IS_NOT_EMPTY()),
 )
