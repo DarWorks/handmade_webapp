fef967db23bc08a0d192712a6c641222dc2bfcc5
jonUCSC <jmunizmu@ucsc.edu>
2022-06-07 11:51:05 -0700

    added creds

--- private/secret_settings.py
+++ private/secret_settings.py
@@ -1,4 +1,4 @@
-DB_USER="handmade",
-DB_NAME="handmade",
-DB_PASSWORD="1q2w3e4r5t",
-DB_CONNECTION=""
+DB_USER="handmade-db"
+DB_NAME="handmade"
+DB_PASSWORD="1q2w3e4r5t"
+DB_CONNECTION="handmade-352523:us-west2:handmade-db"
